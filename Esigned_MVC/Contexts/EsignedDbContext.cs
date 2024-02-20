7e1edce64bd542a72bab2aef81c42a3e043d4403
AydinovMurad <muradmay@code.edu.az>
2024-02-20 13:38:15 +0400

    ready

---
+++ Esigned_MVC/Contexts/EsignedDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Esigned.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Esigned_MVC.Contexts
+{
+    public class EsignedDbContext : DbContext
+    {
+        public EsignedDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Slider> Sliders { get; set; } = null!;
+        public DbSet<Service> Services { get; set; } = null!;
+        public DbSet<Setting> Settings { get; set; } = null!;
+        public DbSet<Position> Positions { get; set; } = null!;
+        public DbSet<Portfolio> Portfolios { get; set; } = null!;
+        public DbSet<Testimonial> Testimonials { get; set; } = null!;
+    }
+}
---
+++ Esigned_MVC/Controllers/ServiceController.cs
@@ -0,0 +1,27 @@
+﻿using Esigned.Core.Entities;
+using Esigned_MVC.Contexts;
+using Esigned_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Esigned_MVC.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly EsignedDbContext _esignedDbContext;
+
+        public ServiceController(EsignedDbContext esignedDbContext)
+        {
+            _esignedDbContext = esignedDbContext;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Service> services = await _esignedDbContext.Services
+                .Where(m => !m.IsDeleted)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(services);
+        }
+    }
+}
